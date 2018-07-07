6b9c9f7835eb2365a6433f15a0c802f69356c2b6
Amy Shen <amyshen@gmail.com>
2018-07-08 02:28:20 +0800

    playing around unsuccessfully with javascript hyperledger api

---
+++ scripts/search_ledger.js
@@ -0,0 +1,26 @@
+const AdminConnection = require('composer-admin').AdminConnection;
+
+let adminConnection = new AdminConnection();
+await adminConnection.connect('card1');
+
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+
+let bizNetConnection = new BusinessNetworkConnection();
+let businessNetworkDefintion = await bizNetConnection.connect('cardNameToUse');
+
+function addNewTrader(userCardName, tid) {
+	// connect using the 'newUserCard', create an asset, add it to a registry and get all assets. 
+	try {
+	  await businessNetworkConnection.connect(userCardName);
+      var factory = getFactory();
+	  let trader = factory.newResource('org.acme.mynetwork','Trader', tid);
+	  await assestRegistry.add(trader);
+
+	  result = await assetRegistry.getAll();
+	  log.info(result);
+
+	  await businessNetworkConnection.disconnect();
+	} catch (error) {
+		log.info("error :(");
+	}
+}
